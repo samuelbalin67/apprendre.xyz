ade3cddd9a63c3728338f10bd93b778f734db41d
samuelbalin67 <samuel.balin67@gmail.com>
2019-11-14 22:19:44 +0100

    sorti une partie des CSS qui étaient dur dans les JS

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -5,21 +5,14 @@ for (i = 0; i < acc.length; i++) {
   /*for all title with description, change pointer and background*/
   var panel = acc[i].nextElementSibling;
   if(panel){
-  	acc[i].style.cursor = "pointer";
-  	acc[i].style.background = "#e6e6e6";
-  	acc[i].style.padding = ".2rem .5rem";
+	acc[i].classList.add("clickable");
   }
   /*when title clicked, if there is a description, show it*/
   acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
+	this.classList.toggle("active");
+	this.nextElementSibling.classList.toggle("shown");
     var panel = this.nextElementSibling;
     if(panel){
-    	console.log(this.style.background);
-    	if(this.style.background === "rgb(230, 230, 230)"){
-    		this.style.background = "white";
-    	} else {
-    		this.style.background = "rgb(230, 230, 230)";
-    	}
     	if (panel.style.display === "block") {
       		panel.style.display = "none";
     	} else {
