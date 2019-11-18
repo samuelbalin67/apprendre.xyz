2abb7e4a4586c74ae91be3cf627994e1a50c39f8
samuelbalin67 <samuel.balin67@gmail.com>
2019-11-18 23:26:09 +0100

    accordion slide down effect finally git status :)

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -5,11 +5,17 @@ for (i = 0; i < acc.length; i++) {
   /*for all title with description, change pointer and background*/
   var panel = acc[i].nextElementSibling;
   if(panel){
-	acc[i].classList.add("clickable");
+	  acc[i].classList.add("clickable");
   }
   /*when title clicked, if there is a description, show it*/
   acc[i].addEventListener("click", function() {
-	this.classList.toggle("active");
-	this.nextElementSibling.classList.toggle("shown");
+    this.classList.toggle("active");
+    /*accordion slide down effect -> manage description's height which seemed impossible in pure css*/
+    description = this.nextElementSibling;
+    if (description.style.maxHeight) {
+      description.style.maxHeight = null;
+    } else {
+      description.style.maxHeight = description.scrollHeight + "px";
+    }
   });
 }
